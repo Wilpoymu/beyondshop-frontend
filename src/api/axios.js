64ec37a58062ce326d52ec41f40a90316cdff10c
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-08-09 04:46:29 -0500

    Automate Accounting

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_URL } from '../config';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_URL,
   withCredentials: true,
   headers: {
     'Content-type': 'application/json',
