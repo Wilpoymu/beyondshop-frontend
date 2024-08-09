fb6f6f55763225808cda68b0223c9445328dfa3d
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-08-09 01:03:15 -0500

    this doesn't really make things faster, but I tried

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,67 @@
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function RegisterPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { signup, isAuthenticated, errors: registerErrors } = useAuth();
+  const navigate = useNavigate;
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/products");
+  }, [isAuthenticated]);
+
+  const onSubmit = handleSubmit(async (values) => {
+    signup(values);
+  });
+
+  return (
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      {registerErrors.map((error, i) => (
+        <div key={i} className="bg-red-500 p-2 text-white rounded-md">{error}</div>
+      ))}
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          {...register("username", {
+            required: "Username is required",
+          })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Username"
+        />
+        {errors.username && (
+          <p className="text-red-500">{errors.username.message}</p>
+        )}
+        <input
+          type="email"
+          {...register("email", {
+            required: "Email is required",
+          })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Email"
+        />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+        <input
+          type="password"
+          {...register("password", {
+            required: "Password is required",
+          })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Password"
+        />
+        {errors.password && (
+          <p className="text-red-500">{errors.password.message}</p>
+        )}
+
+        <button type="submit">Registrar</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
