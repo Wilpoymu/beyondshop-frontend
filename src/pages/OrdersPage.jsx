59bdacac2036cb43a2c0addbb220f76884cfe23b
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-08-26 00:39:33 -0500

    refactor: Update CreateOrdersPage to use OrderContext for creating orders and calculate total price

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,28 +1,45 @@
 import { useEffect } from 'react';
 import OrderCard from '../components/OrderCard';
 import { useOrder } from '../context/OrderContext';
+import { Link } from 'react-router-dom';
+import '../index.css'; // Asegúrate de importar el archivo de estilos
 
 function OrdersPage() {
   const { getOrders, orders } = useOrder();
 
-
   useEffect(() => {
     getOrders();
-  }, []);
+  }, [getOrders]);
 
   if (!orders.length) {
     return (
-      <div className="flex justify-center items-center h-full">
-        <h1 className="text-2xl font-bold text-gray-500">No orders</h1>
-      </div>
+      <>
+        <div className="flex justify-center items-center h-full">
+          <h1 className="text-2xl font-bold text-gray-500">No orders</h1>
+        </div>
+        <Link
+          className="fixed-bottom-right"
+          to="/create-orders"
+        >
+          Create an order
+        </Link>
+      </>
     );
   }
 
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
-      {orders.map((order) => (
-        <OrderCard key={order._id} order={order} />
-      ))}
+    <div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
+        {orders.map((order) => (
+          <OrderCard key={order._id} order={order} />
+        ))}
+      </div>
+      <Link
+        className="fixed-bottom-right"
+        to="/create-orders"
+      >
+        Create an order
+      </Link>
     </div>
   );
 }
